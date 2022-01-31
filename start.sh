98af53ed5b8624bc951a8761daef0b6e8600e0cc
DevComp <74418041+TheAwesomeCoder05@users.noreply.github.com>
2022-01-31 16:44:07 +0530

    Start Shell Scripts

---
+++ start.sh
@@ -0,0 +1,10 @@
+## install jabba with Java 16
+echo Using Java 16...
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
+## get java 16
+jabba install zulu@1.16.0
+echo Java is installed.
+java -version
+npm i
+echo Donwloading latest lavalink jar...
+node index
